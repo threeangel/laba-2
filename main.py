1bb25bd989c743b0b48bbb4615609dab7b9ff0bb
evshu <evshurikov@mail.ru>
2023-03-03 03:44:26 +0300

    zad1

---
+++ main.py
@@ -0,0 +1,7 @@
+password = input("Enter password: ")
+confirm_password = input("Confirm password: ")
+
+if password == confirm_password:
+    print("Password accepted")
+else:
+    print("Password not accepted")
